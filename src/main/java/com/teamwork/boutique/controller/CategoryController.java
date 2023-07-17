afe1c242d89dae5c3e05f29513c318ec73f400d1
Thuc6ngon <tranthuc04122003@gmail.com>
2023-07-18 00:15:01 +0700

    update corsx category

--- src/main/java/com/teamwork/boutique/controller/CategoryController.java
+++ src/main/java/com/teamwork/boutique/controller/CategoryController.java
@@ -10,21 +10,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.logging.Logger;
 
 @RestController
+@CrossOrigin(value = "*")
+
 @RequestMapping("/category")
 public class CategoryController {
     @Autowired
     private ProductServiceImp productServiceImp;
     @Autowired
     private CategoryServiceImp categoryServiceImp;
-
 
     @GetMapping("")
     public ResponseEntity<?> getAllCategory(){
@@ -33,7 +31,6 @@ public class CategoryController {
     response.setData(categoryServiceImp.getAllCategory());
     return new ResponseEntity<>(response, HttpStatus.OK);
 }
-
     @GetMapping("/{id}")
     public ResponseEntity<?>getProductByCategory(@PathVariable int id){
 
