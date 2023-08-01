70eff8738a5fff40805a4afa029a2b54ce67741e
xiucaliu <lelinh050896@gmail.com>
2023-08-01 15:23:02 +0700

    update_cart_4

---
+++ src/main/java/com/teamwork/boutique/payload/request/CartUpdateRequest.java
@@ -0,0 +1,26 @@
+package com.teamwork.boutique.payload.request;
+
+public class CartUpdateRequest {
+    private int id;
+    private int quantity;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public CartUpdateRequest(){};
+
+
+}
--- src/main/java/com/teamwork/boutique/service/CartService.java
+++ src/main/java/com/teamwork/boutique/service/CartService.java
@@ -4,6 +4,8 @@ import com.google.gson.Gson;
 import com.teamwork.boutique.entity.CartEntity;
 import com.teamwork.boutique.entity.ProductEntity;
 import com.teamwork.boutique.entity.StockEntity;
+import com.teamwork.boutique.entity.UserEntity;
+import com.teamwork.boutique.payload.request.CartUpdateRequest;
 import com.teamwork.boutique.payload.response.CartResponse;
 import com.teamwork.boutique.repository.CartRepository;
 import com.teamwork.boutique.repository.ProductRepository;
@@ -26,24 +28,25 @@ public class CartService implements CartServiceImp {
     private UserRepository userRepository;
     @Autowired
     private ProductRepository productRepository;
+
     @Override
-    public boolean addToCart(int productId, int colorId, int quantity) {
+    public boolean addToCart(int productId, int colorId, int quantity, int userId) {
         boolean isSuccess = false;
         StockEntity stock = stockRepository.findByProductIdAndColorId(productId, colorId);
+        UserEntity user = userRepository.findById(userId);
         try {
-            if (stock != null) {
+            if (stock != null && user != null) {
                 System.out.println(stock.getQuantity() + " product Quantity");
-                CartEntity cart = cartRepository.findByStockId(stock.getId());
-                if (cart == null) {//nếu product chưa tồn tại
+                CartEntity cart = cartRepository.findByStockIdAndUserId(stock.getId(), user.getId());
+                if (cart == null) {//nếu stock chưa tồn tại
                     cart = new CartEntity();
                     System.out.println(cart + " cart is null?");
                     cart.setStock(stock);
+                    cart.setUser(user);
                     cart.setQuantity(quantity);
+                } else {
+                    cart.setQuantity(cart.getQuantity() + quantity);
                 }
-                else{
-                    cart.setQuantity(cart.getQuantity()+quantity);
-                }
-
                 cartRepository.save(cart);
                 return isSuccess = true;
             }
@@ -52,11 +55,13 @@ public class CartService implements CartServiceImp {
         }
         return isSuccess;
     }
+
     @Override
-    public List<CartResponse> getAllCart() {
+    public List<CartResponse> getAllCart(int userId) {
         System.out.println("Kiem tra category");
         List<CartResponse> listResponse = new ArrayList<>();
-        List<CartEntity> list = cartRepository.findAll();
+        UserEntity user = userRepository.findById(userId);
+        List<CartEntity> list = cartRepository.findByUserId(user.getId());
         for (CartEntity data : list) {
             CartResponse response = new CartResponse();
             response.setId(data.getId());
@@ -64,11 +69,50 @@ public class CartService implements CartServiceImp {
             ProductEntity product = productRepository.findById(stock.getProduct().getId());
             response.setProductName(product.getName());
             response.setStockPrice(stock.getPrice());
+            response.setStockImage(stock.getImage());
             response.setQuantity(data.getQuantity());
             listResponse.add(response);
         }
         Gson gson = new Gson();
         String data = gson.toJson(listResponse);
         return listResponse;
+    }
+/*
+    @Override
+    public int countCartItems(String email) {
+        UserEntity user = userRepository.findByEmail(email);
+        List<CartEntity> allCartList = cartRepository.findByUserId(user.getId());
+        int totalQuantity =0;
+        for(CartEntity cart:allCartList){
+            totalQuantity+=cart.getQuantity();
+        }
+        return allCartList.size();
+    }*/
+
+    @Override
+    public boolean delete(int cartId) {
+        boolean isSuccess = false;
+        try {
+            CartEntity cart = cartRepository.deleteById(cartId);
+            isSuccess = true;
+        } catch (Exception e) {
+            System.out.println("Lỗi cart delete " + e.getMessage());
+        }
+        return isSuccess;
+    }
+
+    @Override
+    public boolean updateCart(int id, int quantity) {
+        boolean isSuccess = false;
+        try {
+            System.out.println(id);
+            CartEntity cartEntity = cartRepository.findById(id);
+            cartEntity.setQuantity(quantity);
+            cartRepository.save(cartEntity);
+            isSuccess = true;
+        } catch (Exception e) {
+            System.out.println("Lỗi addToCart " + e.getMessage());
+        }
+        return isSuccess;
     }
 }
