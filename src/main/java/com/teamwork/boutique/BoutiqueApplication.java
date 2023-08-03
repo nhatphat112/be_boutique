ba06e01c8f7b34585562397de6fdba773076ad59
nhatphat112 <127173620+nhatphat112@users.noreply.github.com>
2023-08-03 16:33:06 +0700

    complete checkout

--- src/main/java/com/teamwork/boutique/BoutiqueApplication.java
+++ src/main/java/com/teamwork/boutique/BoutiqueApplication.java
@@ -2,6 +2,9 @@ package com.teamwork.boutique;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@EnableTransactionManagement
 
 @SpringBootApplication
 public class BoutiqueApplication {
